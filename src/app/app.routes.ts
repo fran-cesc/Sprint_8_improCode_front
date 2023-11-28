ffc6e13f756729a14ace069873f117c7ac48052f
Cesc <francesc9000@gmail.com>
2023-11-28 21:29:37 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './pages/map/map.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { GraphComponent } from './pages/graph/graph.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'map',
+    component: MapComponent
+  },
+  {
+    path: 'calendar',
+    component: CalendarComponent
+  },
+  {
+    path: 'graph',
+    component: GraphComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/pages/graph/graph.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-graph',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './graph.component.html',
+  styleUrl: './graph.component.css'
+})
+export class GraphComponent {
+
+}
